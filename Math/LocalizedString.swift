34cd9c429f213df7c486b31201ac1716521f48e6
SirarpiBayramyan <you@example.com>
2024-08-05 14:55:21 +0400

    added possibility to change app langauage dynamically

--- Math/LocalizedString.swift
+++ Math/LocalizedString.swift
@@ -2,48 +2,23 @@
 import Foundation
 
 public extension String {
-  
-  /// Localized string
-  /// - Parameter args: args for string
+  /// Localized string with arguments
+  /// - Parameter args: arguments for the string
   /// - Returns: localized string
-  func localized(args: String...) -> String {
+  func localized(args: CVarArg...) -> String {
     return String(format: localized(), arguments: args)
   }
-  
-  /// Localized string
-  /// - Parameter arg: argument for string
+
+  /// Localized string with a single argument
+  /// - Parameter arg: argument for the string
   /// - Returns: localized string
-  func localized(arg: String) -> String {
+  func localized(arg: CVarArg) -> String {
     return String(format: localized(), arguments: [arg])
   }
-  
-  /// Localized string
+
+  /// Localized string without arguments
   /// - Returns: localized string
   func localized() -> String {
     return NSLocalizedString(self, bundle: Bundle.main, comment: "")
-  }
-  
-}
-
-extension Bundle {
-  static var bundleKey: UInt8 = 0
-  
-  static func setLanguage(_ language: String) {
-    defer {
-      object_setClass(Bundle.main, BundleEx.self)
-    }
-    
-    objc_setAssociatedObject(Bundle.main, &bundleKey, Bundle.main.path(forResource: language, ofType: "lproj"), .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-  }
-}
-
-private class BundleEx: Bundle {
-  override func localizedString(forKey key: String, value: String?, table tableName: String?) -> String {
-    guard let bundlePath = objc_getAssociatedObject(self, &Bundle.bundleKey) as? String,
-          let bundle = Bundle(path: bundlePath) else {
-      return super.localizedString(forKey: key, value: value, table: tableName)
-    }
-    
-    return bundle.localizedString(forKey: key, value: value, table: tableName)
   }
 }
--- Math/MathApp.swift
+++ Math/MathApp.swift
@@ -3,11 +3,27 @@ import SwiftUI
 
 @main
 struct MathApp: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @StateObject private var languageManager = LanguageManager()
+
     var body: some Scene {
         WindowGroup {
-          MainView()
-            .environmentObject(LoadingManager.shared)
+            MainView()
+                .environmentObject(languageManager)
+                .environmentObject(LoadingManager.shared)
         }
+    }
+  
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        Bundle.setLanguage(Bundle.currentLanguage())
+        return true
     }
 }
 
